c2224341bdf95d90cf22deb872a17b5d4d2bb9c3
lenindelarosa <leninmanueld@gmail.com>
2022-02-18 10:16:45 -0400

    adding sass file

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -10,32 +10,24 @@ const HomePage = () => (
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
-    </div>
-    <div className="directory-menu">
         <div className="menu-item">
             <div className="content">
                 <h1 className="title">JACKETS</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
-    </div>
-    <div className="directory-menu">
         <div className="menu-item">
             <div className="content">
                 <h1 className="title">SNEAKERS</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
-    </div>
-    <div className="directory-menu">
         <div className="menu-item">
             <div className="content">
                 <h1 className="title">WOMEN</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
-    </div>
-    <div className="directory-menu">
         <div className="menu-item">
             <div className="content">
                 <h1 className="title">MEN</h1>
