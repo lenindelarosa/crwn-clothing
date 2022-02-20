e2bc9369bbe14f949381d2ba655160aee57921de
lenindelarosa <leninmanueld@gmail.com>
2022-02-20 18:02:34 -0400

    adding hats route

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './homepage.styles.scss'
 import Directory from "../../components/directory/directory.component";
+import { Route, Link } from 'react-router-dom'
 
 const HomePage = () => (
 <div className="homepage">
-    <Directory/>
+    <Directory />
 </div>
 );
 
