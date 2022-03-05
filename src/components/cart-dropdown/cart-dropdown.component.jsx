c1010d95d83cb59f2940e2bb511d051e698e84b6
lenindelarosa <leninmanueld@gmail.com>
2022-03-05 14:43:07 -0400

    adding add to cart action

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CustomButton from "../custom-button/custom-botton.component";
+import CustomButton from "../custom-button/custom-button.component";
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => (
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-botton.component";
+import CustomButton from "../custom-button/custom-button.component";
 import './sign-up.styles.scss'
 import { auth, createUserProfiledocument } from "../../firebase/firebase.utils";
 
